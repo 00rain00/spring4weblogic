048570f0cb60a32612877cfbdb10c028a3a8d957
yulin <yulin.cai@dxc.com>
2020-07-14 21:33:54 +0900

    hhhh

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -11,10 +13,21 @@ import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
+
+
+
 @RestController
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication  extends SpringBootServletInitializer implements WebApplicationInitializer {
 
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	return application.sources(DemoApplication.class);
+	}
 	public static void main(String[] args)throws Exception {
 		SpringApplication.run(DemoApplication.class, args);
 	}
